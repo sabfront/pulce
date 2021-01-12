4440930594bfb7a1441651ca2e724812f9c4ef8c
Sabina Shaim <sab.front@gmail.com>
2021-01-13 01:29:02 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -8,25 +8,25 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+// $mail->SMTPDebug = 3;                               
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
+$mail->isSMTP();                                      
+$mail->Host = 'smtp.gmail.com';  
+$mail->SMTPAuth = true;                             
+$mail->Username = 'sab.front@gmail.com';                
+$mail->Password = 'SabinaShaim2020';                           
+$mail->SMTPSecure = 'ssl';                            
+$mail->Port = 465;                                    
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->setFrom('sab.front@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addAddress('ellen@example.com');              
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+//$mail->addAttachment('/var/tmp/file.tar.gz');        
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
+$mail->isHTML(true);                                 
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
